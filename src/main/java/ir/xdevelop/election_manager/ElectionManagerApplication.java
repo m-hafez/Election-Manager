fd004792f54d103c40102c6efc23d74c33aa4855
m-hafez <hafez.yari33@gmail.com>
2019-04-09 17:34:06 +0430

    base commit

--- src/main/java/ir/xdevelop/election_manager/ElectionManagerApplication.java
+++ src/main/java/ir/xdevelop/election_manager/ElectionManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElectronicVotingApplication {
+public class ElectionManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ElectronicVotingApplication.class, args);
+        SpringApplication.run(ElectionManagerApplication.class, args);
     }
 
 }
